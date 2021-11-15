7577f0375e17b09aaec82fe45cbcb1894f12de26
luhaifeng <lhf222458@ncarzone.com>
2021-11-15 09:09:37 +0800

    feat: 添加favicon配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,77 +1,22 @@
-const fs = require('fs')
-const BASE_URL = './docs/views/'
-
-let getFiles = function(type) {
-	let baseUrl = type && `${type.split('/')[1]}/`
-	let urls = fs.readdirSync(`${BASE_URL}${type}`).map(item => {
-		return baseUrl + item
-	})
-	return urls
-}
-let sidebar = {}
-const pageConfig = [
-	// {
-	// 	name: '/views/CSS3Note/',
-	// 	baseUrl: 'CSS3Note/',
-	// 	pathes: ['', {
-	// 		name: '背景与边框',
-	// 		url: 'backgroundAndBorder'
-	// 	}, {
-	// 		name: '201907归档',
-	// 		url: '201907'
-	// 	}]
-	// },
-	// {
-	// 	name: '/views/JSNote/',
-	// 	baseUrl: 'JSNote/',
-	// 	pathes: ['', {
-	// 		name: '201907归档',
-	// 		url: '201907'
-	// 	},{
-	// 		name: 'Nodejs',
-	// 		url: 'Nodejs'
-	// 	},{
-	// 		name: '数据结构与算法',
-	// 		url: 'DataStructureAndAlgorithm'
-	// 	},{
-	// 		name: 'TypeScript',
-	// 		url: 'TypeScript'
-	// 	}]
-	// }
-]
-pageConfig.forEach(item => {
-	sidebar[item.name] = []
-	item.pathes.forEach(path => {
-		if(path) {
-			let children = getFiles(`${item.baseUrl}${path.url}`)
-			let conf = {
-				title: path.name,
-				collapsable: false,
-				children
-			}
-			sidebar[item.name].push(conf)
-		} else {
-			sidebar[item.name].push(path)
-		}
-	})
-})
-
 let config = {
 	title: 'vue3-study',
 	description: ' ',
 	base: '/vue3-study/',
+	head: [
+		['link', { rel: 'icon', href: '/images/logo.svg' }]
+	],
 	markdown: {
 		lineNumbers: true
 	},
 	themeConfig: {
 		lastUpdated: 'Last Updated',
 		sidebarDepth: 2,
-		sidebar: 'auto',
+		// sidebar: 'auto',
 		nav: [
 			{ text: 'Home', link: '/' },
 			{ text: 'GitHub', link: 'https://github.com/luhaifeng666/vue3-study' },
 		],
-		sidebar
+		// sidebar
 	}
 }
 
