53448b8ea1bcaa159a65f181baa6d348cd940379
ext.luhaifeng1 <ext.luhaifeng1@jd.com>
2022-06-13 09:21:22 +0800

    chore: 移除不必要的文件

--- docs/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/Users/luhaifeng/code/mine/vue3-study/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-beta.35/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- docs/.vuepress/.temp/internal/clientAppSetups.js
+++
@@ -1,9 +0,0 @@
-import clientAppSetup0 from '/Users/luhaifeng/code/mine/vue3-study/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-beta.35/node_modules/@vuepress/plugin-active-header-links/lib/client/clientAppSetup.js'
-import clientAppSetup1 from '/Users/luhaifeng/code/mine/vue3-study/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-beta.35/node_modules/@vuepress/plugin-nprogress/lib/client/clientAppSetup.js'
-import clientAppSetup2 from '/Users/luhaifeng/code/mine/vue3-study/node_modules/.pnpm/@vuepress+theme-default@2.0.0-beta.35/node_modules/@vuepress/theme-default/lib/client/clientAppSetup.js'
-
-export const clientAppSetups = [
-  clientAppSetup0,
-  clientAppSetup1,
-  clientAppSetup2,
-]
