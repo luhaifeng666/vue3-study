dd1bbe3c7277ad017e16ce418d3ea3d6c5e594a8
luhaifeng666 <youzui@hotmail.com>
2022-07-05 14:57:35 +0800

    feat: 实现 computed

---
+++ src/reactivity/computed.ts
@@ -0,0 +1,33 @@
+/*
+ * @Author: luhaifeng666 youzui@hotmail.com
+ * @Date: 2022-07-05 14:25:24
+ * @LastEditors: luhaifeng666
+ * @LastEditTime: 2022-07-05 14:57:17
+ * @Description: 
+ */
+import { ReactiveEffect } from './effect'
+
+class ComputedRefImpl {
+  private _getter: any
+  private _dirty: boolean = true // 标记是否需要触发 getter
+  private _value: any // 缓存值
+  private _effect: ReactiveEffect
+  
+  constructor(getter) {
+    this._getter = getter
+    this._effect = new ReactiveEffect(this._getter, () => {
+      if (!this._dirty) this._dirty = true
+    })
+  }
+
+  get value() {
+    // 值没有发生变化时，再次获取不会触发 getter
+    if (this._dirty) {
+      this._dirty = false
+      this._value = this._effect.run()
+    }
+    return this._value
+  }
+}
+
+export const computed = getter => new ComputedRefImpl(getter)
